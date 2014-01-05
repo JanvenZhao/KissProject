8cdce83d06614086a9c9b4f7b1ce9752d2c25469
JanvenZhao <janvenzhao@gmail.com>
2014-01-05 17:20:05 +0800

    add InputView and InputAccessoryView

--- TableTest/TableTest/CustomTextField.m
+++ TableTest/TableTest/CustomTextField.m
@@ -29,16 +29,26 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
     
+    
+    UIView *accessory = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 40)];
+    accessory.backgroundColor = [UIColor redColor];
+    
+    //_textField.inputView  =accessory;
+    //_textField.inputAccessoryView = accessory;
+    
     _textField = [[CTField alloc] initWithFrame:CGRectMake(20, 50, 280, 40)];
     _textField.placeholder = @"新建一项";
     _textField.borderStyle = UITextBorderStyleRoundedRect;
     _textField.textAlignment = NSTextAlignmentLeft;
     _textField.delegate = self;
+   
     _textField.clearButtonMode = UITextFieldViewModeWhileEditing;
     UIImageView *imgv = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"qq.png"]];//23*23.5
     _textField.leftView = imgv;
     _textField.leftViewMode = UITextFieldViewModeAlways;
     [self.view addSubview:_textField];
+    
+    
     
     
 }
